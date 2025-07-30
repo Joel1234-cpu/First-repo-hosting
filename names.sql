318be91f37ebf2608e9ebb7fa7ca34473fcd3b85
Joel Koshy <joelbijukoshy2005@gmail.com>
2025-07-30 07:19:58 +0530

    Updated index.html

--- names.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE predictions (
-    id INT not null AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    predicted_gender VARCHAR(10),
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-);
